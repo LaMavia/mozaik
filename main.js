514cfccf937f9613cd4c40881afdb925f61bf008
LastShadowPL <xeloxpl@windowslive.com>
2017-10-06 22:58:46 +0200

    added inputs !no labels

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
-var amount = 300, out = document.querySelector('div.photo');
-function rc() {
-    return Math.floor(Math.random() * (255 - 50) + 50);
+const amount = 3000, out = document.querySelector('div.photo'), input = document.querySelector('input.input-link'), inNum = document.querySelector('input.input-num');
+let link = './cat.jpg';
+let size = 2;
+let styles = [
+    `--link: url(${link})`
+];
+function upStyles() {
+    styles = [
+        `--link: url(${link})`,
+        `--s: ${size || 2}`
+    ];
+    const mStyles = styles.join("; ");
+    out.setAttribute('style', mStyles);
 }
-for (var i = 0; i < amount; i++) {
-    var el = document.createElement('div'), _a = [rc(), rc(), rc()], r = _a[0], g = _a[1], b = _a[2];
-    el.classList.add('glass');
-    el.style.setProperty('--color', "rgb(" + r + "," + g + "," + b + ")");
-    out.appendChild(el);
+upStyles();
+function rc(min = 100, max = 255) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
+function setLink(l) {
+    link = l || './cat.jpg';
+    upStyles();
+}
+function setSize(s) {
+    size = s;
+    upStyles();
+}
+window.addEventListener('DOMContentLoaded', (e) => {
+    for (let i = 0; i < amount; i++) {
+        const el = document.createElement('div'), [r, g, b] = [rc(), rc(), rc()];
+        el.classList.add('glass');
+        el.style.setProperty('--color', `rgb(${r},${g},${b})`);
+        out.appendChild(el);
+    }
+});
+inNum.addEventListener('change', function (e) {
+    setSize(e.target.value);
+});
+input.addEventListener('change', function (e) {
+    setLink(e.target.value);
+});
--- main.ts
+++ main.ts
@@ -1,12 +1,46 @@
-const amount:number = 300,
-  out:Element = document.querySelector('div.photo')
-function rc():number{
-  return Math.floor(Math.random() * (255 - 50) + 50)
+const amount:number = 3000,
+  out:Element = document.querySelector('div.photo'),
+  input:Element = document.querySelector('input.input-link'),
+  inNum:Element = document.querySelector('input.input-num')
+let link:string = './cat.jpg'
+let size = 2;
+let styles = [
+  `--link: url(${link})`
+]
+
+function upStyles():void{
+  styles = [
+    `--link: url(${link})`,
+    `--s: ${size || 2}`
+  ]
+  const mStyles = styles.join("; ")
+  out.setAttribute('style', mStyles)
 }
-for(let i = 0;i < amount;i++){
-  const el = document.createElement('div'),
-        [r,g,b]:number[] = [rc(),rc(),rc()]
-  el.classList.add('glass')
-  el.style.setProperty('--color', `rgb(${r},${g},${b})`)
-  out.appendChild(el)
+upStyles()
+function rc(min:number = 100, max:number = 255):number{
+  return Math.floor(Math.random() * (max - min) + min)
 }
+function setLink(l:string):void{
+  link = l || './cat.jpg'
+  upStyles()
+}
+function setSize(s:number):void{
+  size = s
+  upStyles()
+}
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  for(let i = 0;i < amount;i++){
+    const el = document.createElement('div'),
+          [r,g,b]:number[] = [rc(),rc(),rc()]
+    el.classList.add('glass')
+    el.style.setProperty('--color', `rgb(${r},${g},${b})`)
+    out.appendChild(el)
+  }
+})
+inNum.addEventListener('change', function(e){
+  setSize(e.target.value)
+})
+input.addEventListener('change', function(e){
+  setLink(e.target.value)
+})
